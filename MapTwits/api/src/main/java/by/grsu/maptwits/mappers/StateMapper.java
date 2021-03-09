72e9d0c897d110f3ff94c4977377995001719733
Kirill <liw.liw510@gmail.com>
2021-03-09 16:16:36 +0300

    lab oop

---
+++ MapTwits/api/src/main/java/by/grsu/maptwits/mappers/StateMapper.java
@@ -0,0 +1,10 @@
+package by.grsu.maptwits.mappers;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class StateMapper {
+    public State stateMap(){
+
+    }
+}
--- MapTwits/api/src/main/java/by/grsu/maptwits/objforjson/StateJson.java
+++ MapTwits/api/src/main/java/by/grsu/maptwits/objforjson/StateJson.java
@@ -1,7 +1,14 @@
 package by.grsu.maptwits.objforjson;
 
+import lombok.*;
+
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class StateJson {
     String name;
     List<List<List<Double>>> polygons;
